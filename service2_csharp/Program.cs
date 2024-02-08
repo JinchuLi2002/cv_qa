99c234dc705e9d96c721ea8372d4aa9404cc32a8
Jinchu Li <jinchuli2002@gmail.com>
2024-02-08 18:01:13 -0500

    added multiple c# server for layering test

---
+++ service2_csharp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Service1CSharp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Start the client part to send an HTTP POST request to the Go server
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Uri serverUri = new Uri("http://go-server:5001/api/send"); // Updated URL
+                Console.WriteLine("Sending HTTP POST request to the Go server");
+
+                while (true) // Run indefinitely
+                {
+                    var number = new Random().Next(1, 100);
+                    Console.WriteLine($"Sending to Go server: {number}");
+
+                    var data = new
+                    {
+                        Data = number // Match the field name in your Go struct
+                    };
+
+                    var jsonString = JsonSerializer.Serialize(data);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                    // Send the HTTP POST request
+                    var response = await httpClient.PostAsync(serverUri, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read and display the response if needed
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine($"Response from Go server: {responseContent}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"HTTP request failed with status code: {response.StatusCode}");
+                    }
+
+                    // Delay for 5 seconds before sending the next request
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                }
+            }
+        }
+    }
+}
